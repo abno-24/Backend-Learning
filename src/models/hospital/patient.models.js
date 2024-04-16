187db6db530c79bbb3937f9cc345fa4b1eefb0f6
Abno <abhinavmaurya605@gmail.com>
2024-04-16 21:09:52 +0530

    Hospital models added

---
+++ src/models/hospital/patient.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["F", "M", "O"],
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    diagnosedWith: {
+      type: String,
+      required: true,
+    },
+    admittedIn: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Patient = mongoose.model("Patient", patientSchema);
