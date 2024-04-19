30bf491581ed3dc9118b7b9c548559748da487ed
Abno <abhinavmaurya605@gmail.com>
2024-04-19 22:40:44 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,16 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       lowercase: true,
       unique: true,
       trim: true,
       required: true,
+      index: true,
     },
     email: {
       type: String,
@@ -16,10 +19,11 @@ const userSchema = new Schema(
       trim: true,
       required: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       trim: true,
       required: true,
+      index: true,
     },
     avatar: {
       type: String,
@@ -44,5 +48,40 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  // If the password field is not modified then don't do anything
+  if (!this.isModified("password")) return next();
+
+  // Only hash when password field is modified
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
