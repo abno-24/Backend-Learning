993107c4f292889c83d4ba40f149f2c7012d698d
Abno <abhinavmaurya605@gmail.com>
2024-04-23 20:55:25 +0530

    worked on user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Register",
-  });
+  const { userName, email, fullName, password } = req.body;
+  console.log(req.body);
+
+  if (
+    [userName, email, fullName, password].some((filed) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 });
 
 export { registerUser };
