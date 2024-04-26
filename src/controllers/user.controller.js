fe44dc4e7618cdae2ed9b3d55d2e9ac581db1aca
Abno <abhinavmaurya605@gmail.com>
2024-04-26 23:21:24 +0530

    Bug fixed in login and logout feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -90,7 +90,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
-  if (!username || !email)
+  if (!(username || email))
     throw new ApiError(400, "username or email is required");
 
   const user = await User.findOne({
