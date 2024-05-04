89f9f524c9a9ddcd9cda093c56d319aca90a3813
Abno <abhinavmaurya605@gmail.com>
2024-05-04 21:45:11 +0530

    bug fix: logout

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,12 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
+    console.log(req);
+
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
