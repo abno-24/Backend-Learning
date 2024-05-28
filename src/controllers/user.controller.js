3c86ba6850fc2b03a0fddc5a8aa1ea57d0138973
Abno <abhinavmaurya605@gmail.com>
2024-05-28 21:36:15 +0530

    Channel profile controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -300,7 +300,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   const channel = await User.aggregate([
     {
       $match: {
-        username: username?.toLowerCase,
+        username: username?.toLowerCase(),
       },
     },
     {
@@ -322,14 +322,41 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     {
       $addFields: {
         subscribersCount: {
-          $size: "$subscribers"
+          $size: "$subscribers",
         },
         channelsSubscribedToCount: {
-          $size: "$subscribedTo"
-        }
-      }
-    }
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
   ]);
+
+  console.log(channel);
+
+  if (!channel?.length) throw new ApiError(404, "Channel does not exist");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched succesfully"));
 });
 
 export {
